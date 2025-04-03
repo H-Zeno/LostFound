0314e0331122cad68ec2f876719276e208567b36
Zeno <zeno.hamers@gmail.com>
2025-04-03 19:31:05 +0200

    increase drawer detection threshold

--- src/data_processing/yolo_integration.py
+++ src/data_processing/yolo_integration.py
@@ -198,7 +198,7 @@ def register_drawers(dir_path: str) -> list:
     detections = [det for subdets in [detections[opt][0] for opt in optimal_images] for det in subdets]
     
     pcd_original = o3d.io.read_point_cloud(os.path.join(dir_path, 'mesh_labeled.ply'))
-    bboxes_3d = detections_to_bboxes(np.asarray(pcd_original.points), detections, threshold=0.8)
+    bboxes_3d = detections_to_bboxes(np.asarray(pcd_original.points), detections, threshold=0.9)
 
     all_bbox_indices = [(np.array(bbox.get_point_indices_within_bounding_box(pcd_original.points)), conf) for bbox, conf in bboxes_3d]
 
